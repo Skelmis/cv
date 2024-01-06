3e14e8fe4d3cf512a3f519de4ecdc046f9d025aa
skelmis <ethanmkh2@gmail.com>
2024-01-06 20:17:59 +1300

    Initial commit

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,180 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import {GitHubIcon, LinkedInIcon, XIcon} from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
-  contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
-    social: [
-      {
-        name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
-        icon: GitHubIcon,
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+    name: "Ethan McKee-Harris",
+    initials: "EMH",
+    location: "Wellington, New Zealand",
+    locationLink: "https://www.google.com/maps/place/Wellington",
+    about:
+        "Ethical hacker and open source developer attempting to make the world a better place one piece of software at a time.",
+    summary:
+        "As a cyber security consultant, I have helped to improve the security posture for many clients in both the public and private sector." +
+        " Within the security industry I currently specialise in testing web applications and auditing the security of voice biometric systems. " +
+        "In my spare time I build software within various domains, each piece serving it's own purpose. Further, " +
+        "I also manage a couple open source communities with the goal of improving the ecosystem for our end users.",
+    avatarUrl: "https://avatars.githubusercontent.com/u/47520067?v=4",
+    personalWebsiteUrl: "https://skelmis.co.nz",
+    contact: {
+        email: "ethanmkh2@gmail.com",
+        // tel: "+",
+        social: [
+            {
+                name: "GitHub",
+                url: "https://github.com/Skelmis",
+                icon: GitHubIcon,
+            },
+            {
+                name: "LinkedIn",
+                url: "https://www.linkedin.com/in/ethan-m-h/",
+                icon: LinkedInIcon,
+            }
+        ],
+    },
+    education: [
+        {
+            school: "The University of Waikato",
+            degree: "Bachelor of Science majoring in Computer Science",
+            start: "2020",
+            end: "2022",
+        },
     ],
-  },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
-  work: [
-    {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
-  ],
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
-  ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
+    work: [
+        {
+            company: "ZX Security",
+            link: "https://zxsecurity.co.nz/",
+            badges: [],
+            title: "Junior Security Consultant → Security Consultant",
+            start: "2023",
+            end: "Present",
+            description:
+                "I work as a security consultant conducting ethical hacking for our clients. My areas of speciality include web application's and voice biometrics.",
+        },
+        {
+            company: "Endace",
+            link: "https://www.endace.com/",
+            badges: [],
+            title: "Test Engineer",
+            start: "2022",
+            end: "2022",
+            description:
+                "Continued to expand the functionality of internal tooling while conducting further automated test development.",
+        },
+        {
+            company: "Endace",
+            link: "https://www.endace.com/",
+            badges: ["Internship"],
+            title: "Software Quality Assurance Intern",
+            start: "2021",
+            end: "2022",
+            description:
+                "I spent 13 weeks working at Endace as an intern in the quality assurance team. Over this\n" +
+                "period, I developed automation tools to take certain task execution times from days to minutes.",
+        },
+    ],
+    skills: [
+        "Web Application Penetration Testing",
+        "Voice Biometric Penetration Testing",
+        "Static Code Analysis",
+        "Python",
+        "Java",
+        "SQL",
+        "Mongo DB",
+        "Websockets",
+        "Infrastructure Management",
+        "Client Interaction's & Technical Reporting"
+    ],
+    projects: [
+        {
+            title: "Dark Haven Photography",
+            techStack: [
+                "Photography",
+            ],
+            description: "I do photography in my spare time under this brand",
+            link: {
+                label: "darkhaven.co.nz",
+                href: "https://darkhaven.co.nz",
+            },
+        },
+        {
+            title: "Various CVE's",
+            techStack: [
+                "Python",
+                "Websockets",
+            ],
+            description: "The only Discord suggestions bot you'll ever need",
+            link: {
+                label: "A list of my CVE's",
+                href: "https://data.skelmis.co.nz/achievements",
+            },
+        },
+        {
+            title: "Discord Anti Spam",
+            techStack: [
+                "Python",
+            ],
+            description: "A Python package for automated moderation of spam within text mediums, primarily Discord.",
+            link: {
+                label: "Discord Anti Spam github",
+                href: "https://github.com/Skelmis/Discord-Anti-Spam",
+            },
+        },
+        {
+            title: "Alaric",
+            techStack: [
+                "Python",
+                "Mongo DB",
+            ],
+            description: "A simplistic yet powerful asynchronous MongoDB query engine",
+            link: {
+                label: "Alaric github",
+                href: "https://github.com/Skelmis/Alaric",
+            },
+        },
+        {
+            title: "Function Cooldowns",
+            techStack: [
+                "Python",
+            ],
+            description: "A simplistic decorator based approach to rate limiting function calls",
+            link: {
+                label: "Function cooldowns github",
+                href: "https://github.com/Skelmis/Function-Cooldowns",
+            },
+        },
+        {
+            title: "Zonis",
+            techStack: [
+                "Python",
+                "Websockets",
+            ],
+            description: "Agnostic & simple inter process communication for Python programs",
+            link: {
+                label: "Zonis github",
+                href: "https://github.com/Skelmis/Zonis",
+            },
+        },
+        {
+            title: "Suggestions bot",
+            techStack: [
+                "Python",
+                "Mongo DB",
+            ],
+            description: "The only Discord suggestions bot you'll ever need",
+            link: {
+                label: "suggestions.gg",
+                href: "https://suggestions.gg/",
+            },
+        },
+        {
+            title: "Nextcord",
+            techStack: [
+                "Python",
+            ],
+            description: "A popular Python wrapper for the Discord API that I help to manage the open source community for",
+            link: {
+                label: "Nextcord github",
+                href: "https://github.com/nextcord/nextcord/",
+            },
+        },
+    ],
 } as const;
